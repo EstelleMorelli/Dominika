31e2cc4a14315db91400da759e5c97e54e476e83
EstelleMorelli <estelle.morelli@oclock.school>
2024-04-19 16:05:53 +0200

    pas ouf les articles

--- app/Views/coaching/definition.tpl.php
+++ app/Views/coaching/definition.tpl.php
@@ -16,3 +16,9 @@
       </div>
     </div>
   </div>
+
+  <div class="card">
+    <h2>Qu'est ce que le coaching sportif ?</h2>
+  <p>Le coaching sportif est un processus d'accompagnement personnalisé visant à aider les individus à atteindre leurs objectifs en matière de condition physique, de santé et de performance sportive. Les coachs sportifs sont des professionnels qualifiés qui fournissent un soutien, des conseils et des programmes d'entraînement adaptés aux besoins spécifiques de chaque personne, que ce soit pour perdre du poids, améliorer leurs performances athlétiques, prévenir les blessures ou simplement adopter un mode de vie plus sain. Le coaching sportif peut se faire en personne ou à distance, et il peut couvrir divers domaines tels que la nutrition, l'entraînement physique, la récupération, la motivation et la gestion du stress.</p>
+  <p class="card-footer"></p>
+</div>
