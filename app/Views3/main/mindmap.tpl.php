f1665f7ae084b46203bbb0df6cdfd19a19dd2c88
EstelleMorelli <estelle.morelli@oclock.school>
2024-04-25 17:33:29 +0200

    pas ouf

--- app/Views3/main/mindmap.tpl.php
+++ app/Views3/main/mindmap.tpl.php
@@ -1,38 +1,125 @@
-<section>
-	<div class="row">
-		<div class="col-md-4 col-sm-6 col-xs-12">
-			<div class="card">
-				<div class="cover item-a">
-					<h1>Little<br>Bonsai</h1>
-					<span class="price">$79</span>
-					<div class="card-back">
-						<a href="#">Add to cart</a>
-						<a href="#">View detail</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 col-sm-6 col-xs-12">
-			<div class="card">
-				<div class="cover item-b">
-					<h1>Tropical<br>Leaf</h1>
-					<span class="price">$35</span>
-					<div class="card-back">
-						<a href="#">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 col-sm-6 col-xs-12">
-			<div class="card">
-				<div class="cover item-c">
-					<h1>Marijuana<br>Chill</h1>
-					<span class="price">$155</span>
-					<div class="card-back">
-						<a href="#">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+<div class="container">
+    <div class="Posturologie">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover">
+                    <h1>POSTUROLOGIE<br>COACHING SPORTIF</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject1">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-b">
+                    <h1>Difficultés<br>d'apprentissage</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject2">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-c">
+                    <h1>Marijuana<br>Chill</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject3">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-a">
+                    <h1>DOuleurs<br>persistantes</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject4">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-c">
+                    <h1>Marijuana<br>Chill</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject5">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-a">
+                    <h1>Little<br>Bonsai</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject6">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-a">
+                    <h1>Little<br>Bonsai</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject7">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-c">
+                    <h1>Marijuana<br>Chill</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subject8">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card">
+                <div class="cover item-a">
+                    <h1>Little<br>Bonsai</h1>
+                    <span class="price"></span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
