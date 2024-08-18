0358499f6ea941016a456adf22877f783a889106
Estelle Morelli <157108643+EstelleMorelli@users.noreply.github.com>
2024-08-18 21:56:37 +0200

    Update posturologie.tpl.php

--- app/Views/articles/posturologie.tpl.php
+++ app/Views/articles/posturologie.tpl.php
@@ -1,8 +1,9 @@
 <article> 
-    <h2>
+    <img class="article--picture" src="../../public/images/posturologie.jpg" alt="posturologie" />
+    <h2 class="article--title">
     La Posturologie : Une Approche Holistique pour Traiter les Douleurs à leur Source
     </h2>
-    <p>
+    <p class="article--text">
     La posturologie étudie le système tonique postural et vise à traiter les causes profondes des douleurs, plutôt que les symptômes. En adoptant une approche holistique, cette discipline explore le corps à travers ses capteurs clés : les yeux, les pieds et la mâchoire. </br>
 Si l'un de ces capteurs est déréglé, ce qui est souvent le cas, cela crée un déséquilibre dans le corps. Le corps, très intelligent, parvient à s'adapter à ce déséquilibre en créant des compensations. Cependant, ces compensations ont souvent un coût, surtout lorsqu'elles s'accumulent (usure anormale des cartilage, douleurs articulaire, tensions musculaire, tendinites fréquentes, mauvaise performance sportif et scolaire…). </br>
 La posturologie étudie le corps dans sa globalité et établit des liens précieux entre diverses disciplines, telles que l'orthoptie, la podologie, l'orthodontie et l'ostéopathie, offrant ainsi une approche intégrée et complète pour améliorer la santé et le bien-être. </br>
