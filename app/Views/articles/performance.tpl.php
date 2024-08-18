06ed3736378c53dfd0cc849e01a23c23be0edbca
Estelle Morelli <157108643+EstelleMorelli@users.noreply.github.com>
2024-08-18 21:55:33 +0200

    Update performance.tpl.php

--- app/Views/articles/performance.tpl.php
+++ app/Views/articles/performance.tpl.php
@@ -1,8 +1,9 @@
 <article> 
-    <h2>
+    <img class="article--picture" src="../../public/images/performance.jpg" alt="performance" />
+    <h2 class="article--title">
     Vous cherchez à améliorer votre performance physique et à réduire les risques de blessures ?
     </h2>
-    <p>
+    <p class="article--text">
     En identifiant et en corrigeant les déséquilibres posturaux, la posturologie permet d'optimiser l'alignement du corps, favorisant ainsi une meilleure coordination, une plus grande stabilité et une réduction des tensions musculaires inutiles. </br>
     Des capteurs déréglés, des schémas moteurs immatures et des réflexes archaïques non intégrés peuvent rendre le système moins efficace, nécessitant plus d'énergie pour fonctionner. </br>
     </p>
