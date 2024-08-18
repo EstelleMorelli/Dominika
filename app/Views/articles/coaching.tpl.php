87e56175552d2048688324b56c3f8a2c90605213
Estelle Morelli <157108643+EstelleMorelli@users.noreply.github.com>
2024-08-18 21:45:43 +0200

    Update coaching.tpl.php

--- app/Views/articles/coaching.tpl.php
+++ app/Views/articles/coaching.tpl.php
@@ -1,8 +1,9 @@
 <article> 
-    <h2>
+      <img class="article--picture" src="../../public/images/coaching.png" alt="coaching"/>
+    <h2 class="article--title">
   Coaching :
     </h2>
-    <p>
+    <p class="article--text">
    Maintenir une bonne santé passe par une activité physique régulière. Si vous souhaitez être accompagnés de manière individuelle et sécurisée, je vous propose mes services dans le confort de votre domicile. Que vous ayez pour objectif de perdre du poids, de vous remuscler ou d'améliorer votre forme globale, cela vous permet de démarrer votre parcours avec de bons schémas moteurs et d'apprendre à continuer seul par la suite. En tant qu'éducatrice, mon objectif est de vous rendre autonome. </br>
    
    </p>
