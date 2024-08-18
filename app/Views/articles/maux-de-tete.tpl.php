802201728364e52625527666730d5abc76c0aa5c
Estelle Morelli <157108643+EstelleMorelli@users.noreply.github.com>
2024-08-18 21:53:47 +0200

    Update maux-de-tete.tpl.php

--- app/Views/articles/maux-de-tete.tpl.php
+++ app/Views/articles/maux-de-tete.tpl.php
@@ -1,8 +1,9 @@
-<article> 
-    <h2>
+<article>
+    <img class="article--picture" src="../../public/images/migraine.jpg" alt="maux de tete et vertiges"
+    <h2 class="article--title">
     Maux de tête/ vertiges :
     </h2>
-    <p>
+    <p class="article--text">
     Les maux de tête et les vertiges peuvent avoir diverses causes. Il est essentiel de consulter des spécialistes médicaux pour un diagnostic précis. Cependant, parfois, un simple déséquilibre sensoriel, comme au niveau de la mâchoire ou de l'oreille interne, peut être à l'origine de ces problèmes. </br>
 La posturologie offre une approche non-invasive pour évaluer et corriger ces déséquilibres, offrant ainsi une solution efficace et durable. Explorez cette connexion pour un mieux-être global. </br>
 
