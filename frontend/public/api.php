c548a027a6fa6130c75600fa9fff8e70cdfa837e
Estelle Morelli <estelle.morelli@oclock.school>
2024-10-05 15:30:23 +0200

    modif api.php

--- frontend/public/api.php
+++ frontend/public/api.php
@@ -1,4 +1,4 @@
 <?php 
-$api_url = 'http://127.0.0.1:8000/api';
-/*$api_url = 'https://backend.dominika-meno.fr/api';*/
+/*$api_url = 'http://127.0.0.1:8000/api';*/
+$api_url = 'https://backend.dominika-meno.fr/api';
 ?>
