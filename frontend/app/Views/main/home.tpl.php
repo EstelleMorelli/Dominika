e1b8bf8307759de8439654a0c96297d8894fb5b0
Estelle Morelli <estelle.morelli@oclock.school>
2024-10-17 15:43:23 +0200

    mindmap entièrement dynamisé, fonctionnel de 6 à 8 articles

--- frontend/app/Views/main/home.tpl.php
+++ frontend/app/Views/main/home.tpl.php
@@ -2,7 +2,24 @@
 require_once __DIR__ . '/../../../public/functions/formatTextToParagraphsHome.php';
 require_once __DIR__ . '/../../../public/functions/formatTextToParagraphs.php';
 require_once __DIR__ . '/../../../public/functions/truncateByWords.php';
-$mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord.php';
+switch(count($articlesListArticles)) {
+    case 4:
+        $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord_4.php';
+        break;
+    case 5:
+        $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord_5.php';
+        break;
+    case 6:
+        $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord_6.php';
+        break;
+    case 7:
+        $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord_7.php';
+        break;
+    case 8:
+        $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord_8.php';
+    break;
+}
+
 ?> 
 <?php foreach ($articlesHomePage as $key => $currentArticle):?>
     <div class="article home">
@@ -34,22 +51,9 @@ $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord.php';
     </div>
 <?php endforeach; ?>
 
+<div class="mindmap--container__mobile" style="position: relative">
+<?php 
 
-<div class="mindmap--container" style="position: relative;">
-<img class="mindmapimg" src="/../images/mindmap.webp" alt="Image" usemap="#cerclesMap">
-
-<div alt="Hernie discale" class="circle" style="top: 6.3%; left: 26.1%; width: 19%; height: 20.2%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"hernie-discale"]);?>'"></div>
-<div alt="Difficultés scolaires" class="circle" style="top: 9%; left: 64%; width: 21.7%; height: 23.9%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"difficultes-scolaires"]);?>'"></div>
-<div alt="Posturologie" class="circle" style="top: 32.1%; left: 33.8%; width: 32.5%; height: 35.8%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"posturologie"]);?>'"></div>
-<div alt="Maux de tête" class="circle" style="top: 40%; left: 11%; width: 19%; height: 20.5%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"maux-de-tete"]);?>'"></div>
-<div alt="Performance" class="circle" style="top: 72.5%; left: 11.5%; width: 19%; height: 20.5%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"performance"]);?>'"></div>
-<div alt="Douleur persistante" class="circle" style="top: 62%; left: 70.5%; width: 19%; height: 20.5%;" onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>"douleurs-persistantes"]);?>'"></div>
-
-</div>
-
-
-<div class="mindmap--container__test" style="position: relative">
-<?php     
     // Initialisation du compteur pour suivre l'index des coordonnées
     $coordIndex = 1;
     // Boucle sur les articles
@@ -65,21 +69,88 @@ $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord.php';
         }
 ?>
     <!-- Cercle avec texte incurvé -->
-    <svg style="position: absolute; top: calc(<?=$top?> - 20px); left: calc(<?=$left?> - 20px);" width="120" height="120">
+    <svg class="curved-text" style="--top: <?=$top?>; --left: <?=$left?>;"> 
         <defs>
             <!-- Définir un cercle invisible pour le texte -->
             <path id="circlePath<?=$coordIndex?>" d="M 60, 60 m -50, 0 a 50,50 0 1,1 100,0 a 50,50 0 1,1 -100,0" />
         </defs>
         
         <!-- Texte incurvé sur le chemin -->
-        <text font-size="7.5" text-anchor="middle" fill="black" letter-spacing="1">
+        <text font-size="11" text-anchor="middle" fill="black" letter-spacing="1">
             <!-- Ajuster l'orientation du texte sur la droite -->
             <textPath href="#circlePath<?=$coordIndex?>" startOffset="25%" method="<?= ($left > '50%') ? 'stretch' : 'align' ?>" side="<?= ($left > '50%') ? 'right' : 'left' ?>">
-                <?=htmlspecialchars($currentArticle['title'])?>
+                <?=htmlspecialchars_decode($currentArticle['title'])?>
             </textPath>
         </text>
     </svg>
         <!-- Cercle avec image en background -->
+    <div alt="<?=$currentArticle['title']?>" class="circle" 
+        style="
+        top: <?=$top?>; 
+        left: <?=$left?>; 
+        background-image: url('/../images/<?=$currentArticle['picture']?>');
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        cursor: pointer;" 
+        onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>$currentArticle['slug']]);?>'">
+    </div>
+    
+<?php 
+    // Incrémentation du compteur pour le prochain article
+    $coordIndex++;
+    endforeach; 
+?>
+    <div alt="posturologie" class="circle" 
+        style="
+        width: 120px;
+        height: 120px;
+        top: 31%; 
+        left: 31%;
+        background-image: url('/../images/posturologie.webp');
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        cursor: pointer;" 
+        onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>'posturologie']);?>'">
+    </div>
+</div>
+
+<div class="mindmap--container__desktop" style="position: relative">
+<?php     
+   
+    // Initialisation du compteur pour suivre l'index des coordonnées
+    $coordIndex = 1;
+    // Boucle sur les articles
+    foreach ($articlesListArticles as $currentArticle):
+        // Si on a une entrée pour cette clé dans $mindmapCoord
+        if (isset($mindmapCoord[$coordIndex])) {
+            $top = $mindmapCoord[$coordIndex]['top'];
+            $left = $mindmapCoord[$coordIndex]['left'];
+        } else {
+            // Valeurs par défaut si pas de coordonnées correspondantes
+            $top = '0%';
+            $left = '0%';
+        }
+?>
+    <!-- Cercle avec texte incurvé -->
+    <svg class="curved-text" style="--top: <?=$top?>; --left: <?=$left?>;"> 
+        <defs>
+            <!-- Définir un cercle invisible pour le texte -->
+            <path id="circlePath1<?=$coordIndex?>" d="M 172.5, 172.5 m -160, 0 a 160,160 0 1,1 320,0 a 160,160 0 1,1 -320,0" />
+
+        </defs>
+        
+        <!-- Texte incurvé sur le chemin -->
+        <text font-size="21" text-anchor="middle" fill="black" letter-spacing="1">
+            <!-- Ajuster l'orientation du texte sur la droite -->
+            <textPath href="#circlePath1<?=$coordIndex?>" startOffset="25%">
+                <?=htmlspecialchars_decode($currentArticle['title'])?>
+            </textPath>
+        </text>
+    </svg>
+    
+    <!-- Cercle avec image en background -->
     <div alt="<?=$currentArticle['title']?>" class="circle" 
         style="
         top: <?=$top?>; 
@@ -96,7 +167,21 @@ $mindmapCoord = require __DIR__ . '/../../../public/datas/mindmap-coord.php';
     $coordIndex++;
     endforeach; 
 ?>
+    <div alt="posturologie" class="circle" 
+        style="
+        width: 350px;
+        height: 350px;
+        top: 33%; 
+        left: 33%;
+        background-image: url('/../images/posturologie.webp');
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        cursor: pointer;" 
+        onclick="window.location.href='<?= $router->generate('article-detail', ['articleSlug'=>'posturologie']);?>'">
+    </div>
 </div>
+
 
 <div class="contact-btn-container">
 <a href="mailto:contact@dominika-meno.fr" class="contact-btn"> Je veux faire mon bilan postural </a>
